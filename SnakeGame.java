f4e935f5609814c0e17a4345277db625bb5123a0
Sebastian Jitaru <73609156+SebastianJitaru@users.noreply.github.com>
2022-02-13 16:25:17 +0100

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,5 @@
+public class SnakeGame {
+    public static void main(String[] args){
+        new GameFrame();//com que no el farem servir no cal atribuir el gameframe a un altra variable
+    }
+}
